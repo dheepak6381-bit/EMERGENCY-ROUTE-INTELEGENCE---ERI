8bfd3dd84d36bcd1218786503356b581b700a35e
dheepak6381-bit <dheepak6381-bit@users.noreply.github.com>
2026-09-23 06:56:56 +0530

    Built web
    Branch: main
    Commit: 509ee53d2c3a8ef1290f775f8413707761f780e7 (dirty)
    
    package:peanut 6.0.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"56f64c26474b70c7bc4d4c8785097499775d59
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "361613280"
+    serviceWorkerVersion: "3452939202"
   }
 });
